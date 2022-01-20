68664dc48c63043c0dbd92252de2cbae69f08865
annamar26 <ag.lo.am26@gmail.com>
2022-01-19 20:21:01 -0600

    last changes

--- blackrock/src/app/components/header/header.component.ts
+++ blackrock/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalLoginComponent } from '../modal-login/modal-login.component';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,7 @@ export class HeaderComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
 	openDialog() {
-    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+    const dialogRef = this.dialog.open(ModalLoginComponent);
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
