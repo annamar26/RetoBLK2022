2b2d5939335247356e6367c0912ec6b3ea118e22
Colette Ordoñez <pcoletteor@gmail.com>
2022-01-21 12:57:57 -0600

    Edit component route

--- blackrock/src/app/app-routing.module.ts
+++ blackrock/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {  path:"", component:HomeComponent, pathMatch:"full"},
   {path:"quiz", component:QuizComponent, pathMatch:"full"},
-  { path: '**', redirectTo:"", pathMatch:'full'}
+  { path:"edit", component:EditProfileComponent, pathMatch:"full"},
+  { path: '**', redirectTo:"", pathMatch:'full'},
 
 ];
 
