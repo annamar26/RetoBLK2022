04f5d7e0e53ff27431745f99333e7b4c0fffb7a9
annamar26 <ag.lo.am26@gmail.com>
2022-01-21 12:50:26 -0600

    add results page with styles

--- blackrock/src/app/app.module.ts
+++ blackrock/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { FifthQuestionComponent } from './components/fifth-question/fifth-question.component'
 import {MatCardModule} from '@angular/material/card';
 import { QuizComponent } from './pages/quiz/quiz.component';
+import { ResultsComponent } from './pages/results/results.component';
 
 
 @NgModule({
@@ -56,7 +57,8 @@ import { QuizComponent } from './pages/quiz/quiz.component';
 
     SecondquestionComponent,
 
-    FifthQuestionComponent
+    FifthQuestionComponent,
+     ResultsComponent
 
 
   ],
