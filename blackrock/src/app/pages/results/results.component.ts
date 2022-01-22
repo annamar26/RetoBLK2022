94c1df457330e27c575dafc88700a2e3467d1826
danitorrel <torreslz.dan@gmail.com>
2022-01-22 14:07:24 -0600

    hover courses

--- blackrock/src/app/pages/results/results.component.ts
+++ blackrock/src/app/pages/results/results.component.ts
@@ -16,6 +16,22 @@ level: any
   ngOnInit(){
     this.userName= sessionStorage.getItem('Nombre')
 		this.level = sessionStorage.getItem("level")
+		const img = document.querySelector("#levelimg") as Element;
+		if(this.level === "Iniciado Jedi"){
+			img.setAttribute("src", "../../../assets/yoda_1.jpg");
+		}
+		if(this.level === "Padawan Jedi"){
+			img.setAttribute("src", "../../../assets/padawan_2.png")
+		}
+		if(this.level === "Caballero Jedi"){
+	img.setAttribute("src", "../../../assets/obi-results.png")
+}
+if(this.level === "Maestro Jedi"){
+	img.setAttribute("src", "../../../assets/mace_windu_4.jpg")
+}
+if(this.level === "Maestro Yoda"){
+	img.setAttribute("src", "../../../assets/yoda_5.jpg")
+}
   }
   openDialog() {
     const dialogRef = this.dialog.open(RegisterComponent);
