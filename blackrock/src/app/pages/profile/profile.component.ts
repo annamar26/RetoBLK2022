aa9c1b6e008d7ff245cc8b77182776f99cc60875
annamar26 <ag.lo.am26@gmail.com>
2022-01-24 20:45:03 -0600

    logic statadistics

--- blackrock/src/app/pages/profile/profile.component.ts
+++ blackrock/src/app/pages/profile/profile.component.ts
@@ -3,77 +3,82 @@ import { ThemePalette } from '@angular/material/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
 import { FakeAPIService } from 'src/app/services/fake-api.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
-import { jsPDF } from "jspdf";
-
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  userEmail= this.userData.getUser()
-	color: ThemePalette = 'accent';
+  userEmail = this.userData.getUser();
+  color: ThemePalette = 'accent';
   mode: ProgressBarMode = 'determinate';
   bufferValue = 30;
-  userName = ""
-  userLevel = ''
-  percentage = 0
-  leftCourses = 0
-  goal= ""
-  coursesToShow = [] as any
- doc = new jsPDF({
-    orientation: 'l',
-   unit: 'mm',
-   format: 'a4',
-   putOnlyUsedFonts:true,
-   precision: 2,
-  
+  userName = '';
+  userLevel = '';
+  percentage = 0;
+  leftCourses = 0;
+  goal = '';
+  coursesToShow = [] as any;
+  doc = new jsPDF({
+    orientation: 'p',
+    unit: 'mm',
+    format: 'a4',
+    putOnlyUsedFonts: true,
   });
-  constructor(private userData: FirebaseService, private apiservice: FakeAPIService) { }
+  constructor(
+    private userData: FirebaseService,
+    private apiservice: FakeAPIService
+  ) {}
 
   ngOnInit() {
-this.getUserData()
-console.log(this.userLevel)
+    this.getUserData();
+    console.log(this.userLevel);
 
-let content = [] as any;
+    let content = [] as any;
 
+    this.apiservice.getAllUsersData().subscribe((data) => {
+      content = data;
+      let texto = '';
+      for (let element of content) {
+        console.log(content.length)
+        let divlength= content.length%4 
+        if (divlength===0) {
+         console.log("hola")
+        }
+        let claves = Object.keys(element); 
+        for (let i = 0; i < claves.length; i++) {
+          let clave = claves[i];
+          texto += `${clave}: ${element[clave]} \n`;
+        }
 
-this.apiservice.getAllUsersData().subscribe((data)=>{
-content = data
-let texto = ""
-for (let element of content) {
-let reglon = JSON.stringify(element)
-let elemento = reglon.replace(',', '\n')
-console.log(elemento)
- texto += `\n ${elemento}`
-  this.doc.text(texto, 10, 10, );
-}
-
-
-})
+        texto += `............................. \n`;
+        this.doc.text(texto, 10, 10);
+      }
+      console.log(content);
+    });
   }
 
-  getUserData(){
+  getUserData() {
     this.userData.getUser().subscribe((user: any) => {
       console.log(user.email);
       this.apiservice.getEmailUser(user.email).subscribe((response: any) => {
-       this.userName =(response[0].name);
-       this.userLevel =(response[0].level)
-       this.percentage = ((response[0].doneCourses)*100)/15
-       this.leftCourses= 15 -(response[0].doneCourses)
-       this.goal = response[0].goal
-       this.apiservice.getLevelData(this.userLevel).subscribe((response: any)=>{
- this.coursesToShow = response
-        console.log(this.coursesToShow)
-      
-     })
-  })})}
- createPdf(){
-  this.doc.save('users-data.pdf')
- }
-  
+        this.userName = response[0].name;
+        this.userLevel = response[0].level;
+        this.percentage = (response[0].doneCourses * 100) / 15;
+        this.leftCourses = 15 - response[0].doneCourses;
+        this.goal = response[0].goal;
+        this.apiservice
+          .getLevelData(this.userLevel)
+          .subscribe((response: any) => {
+            this.coursesToShow = response;
+            console.log(this.coursesToShow);
+          });
+      });
+    });
+  }
+  createPdf() {
+    this.doc.save('users-data.pdf');
+  }
 }
-
-
-
